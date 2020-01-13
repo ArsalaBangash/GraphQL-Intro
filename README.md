f0a4d0e815def7b54411f6d438a4e39d1290e902
ArsalaBangash <arsalabangash@gmail.com>
2020-01-13 11:26:00 -0500

    Added more guiding comments and logged server URL

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GraphQL-Intro
+
+An introduction to GraphQL through a simple Apollo server. 
+
+The guiding comments were taken from [Newline](https://www.newline.co/)
--- index.js
+++ index.js
@@ -20,15 +20,38 @@ const courses = [
  */
 const typeDefs = gql` type Course { code: String! title: String! term: String! }  type Query { courses: [Course!]! } `;
 
+/**
+ * Our schema simply represents the shape of data that can be queried. 
+ * To define how the fields in the schema get processed, we'll create our 
+ * GraphQL resolvers. Resolvers in a GraphQL API are functions responsible 
+ * for resolving a GraphQL operation to data.
+ * 
+ * We'll specify a resolvers map to dictate how the courses field is to resolve.
+ *  We'll have the courses field simply return the mock courses array we've created.
+ */
 const resolvers = {
     Query: {
         courses: () => courses,
     }
 }
 
+/**
+ * With our schema, resolvers, and mock data defined - we can now create our Apollo 
+ * Server instance. To do so, we'll import and use the ApolloServer constructor
+ * function from the apollo-server library.
+ */
 const { ApolloServer } = require('apollo-server');
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(() => {
-    console.log(`Server is running`);
+/**
+ * With the Apollo server instance now available to us, we can start 
+ * our web server by running the listen() function available as a 
+ * property of the Apollo server. 
+ * 
+ * The server.listen() function is a promise that when resolved receives a 
+ * serverInfo object which has the url of the running server. We'll log a 
+ * message to the console with this url value.
+ */
+server.listen().then((serverInfo) => {
+    console.log(`Server is running at ${serverInfo.url}`);
 });
